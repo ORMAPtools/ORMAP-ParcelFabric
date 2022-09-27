0ae9f9da2bfe0fe8b46166050f2222cf410e73b8
Brian H Wilson <bwilson@co.clatsop.or.us>
2022-09-27 16:49:38 -0700

    changes for version 3

--- T7-4/Fabric/FabricConversion/Relocate.py
+++
@@ -1,8 +0,0 @@
-arcpy 
-import arcpy
-aprx = arcpy.mp.ArcGISProject(r'P:\GISPROJECTS\ASSESSOR\NewCartography\StateView1\OrMapPF.aprx')
-
-aprx.updateConnectionProperties(r'P:\GISPROJECTS\ASSESSOR\NewCartography\StateView1\TownEd.gdb',
-                                r'P:\AIGIS\libraries\taxmap\County\Fabric\TownEd.gdb')
-
-aprx.saveACopy(r'P:\GISPROJECTS\ASSESSOR\NewCartography\StateView1\OrMapPF2.aprx')
